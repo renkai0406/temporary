8cf10da2226639306c39c92522286411cc100369
renkai.phong <renkai.phong@bytedance.com>
2019-02-17 17:17:45 +0800

    create pipeline.

--- Vulkan/Vulkan/src/shadermng.h
+++ Vulkan/Vulkan/src/shadermng.h
@@ -8,18 +8,19 @@
 
 #include "appmng.h"
 
-#define VULKAN_BIN_DIR "D:/VulkanSDK/1.1.85.0/Bin32"
-
 class ShaderManager
 {
 public:
 	ShaderManager();
 	~ShaderManager();
 
-	void init(const VkDevice& device, const std::string* files, int count);
+	void init(const VkDevice& device, const std::string* files, int count, const VkExtent2D& extent);
+	void clearup();
 
 private:
+	VkPipelineLayout pipelineLayout;
 	VkDevice device;
+	VkExtent2D extent;
 	static std::vector<char> readFile(const std::string& fname);
 	VkShaderModule createShaderModule(const std::vector<char>& codes);
 	void createPippline(VkShaderModule& vert, VkShaderModule& geom);
