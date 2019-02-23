ee8917abb4c902dc7450c97cc47cebaca8141442
renkai.phong <renkai.phong@bytedance.com>
2019-02-23 18:45:30 +0800

    create render path.

--- Vulkan/Vulkan/src/vkloader.cpp
+++ Vulkan/Vulkan/src/vkloader.cpp
@@ -354,6 +354,17 @@ void VulkanLoader::createImageViews()
 
 void VulkanLoader::createReanderPass()
 {
+	VkAttachmentDescription colorAttachment = {};
+	colorAttachment.format = vkInfo.scFormat;
+	colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+	colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+	colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+	colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+
 }
 
 void VulkanLoader::createPippline(const std::string* files, int count)
